84e4b41c787b0ad8f04a4fa6998daa06a236ba0b
Janine Tiefenbruck <jlobue@eng.ucsd.edu>
2025-10-06 06:10:12 -0700

    Update lec05_imports.py

--- resources/lectures/lec05/lec05_imports.py
+++ resources/lectures/lec05/lec05_imports.py
@@ -14,7 +14,7 @@ pd.set_option("display.precision", 2)
 from IPython.display import display, IFrame, YouTubeVideo
 
 def show_grouping_animation():
-    src = "https://docs.google.com/presentation/d/e/2PACX-1vTgVlFngQcLMYHP-z1vq5lVXjsBgcHebc-3TX7SW6L_gjX6TD1gsflvVDQUpWiDdeEPqJASenUIfBVd/embed?start=false&loop=false&delayms=60000&rm=minimal"
+    src = "https://docs.google.com/presentation/d/1IBMwlqBtnD-Ml1Zhmqs6pnYV6jcYayr3VGFqN9Dg3xk/embed?start=false&loop=false&delayms=60000&rm=minimal"
     width = 960
     height = 509
     display(IFrame(src, width, height))
