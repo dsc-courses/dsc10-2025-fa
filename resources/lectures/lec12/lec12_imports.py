7c31eff60018012402157fae087687ca476a5497
Kate Feng <164668941+katefengx@users.noreply.github.com>
2025-10-22 00:06:45 -0700

    publishing lecture 12

---
+++ resources/lectures/lec12/lec12_imports.py
@@ -0,0 +1,28 @@
+import numpy as np
+import babypandas as bpd
+
+
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-colorblind')
+plt.rcParams.update({
+    'figure.figsize': (10, 5),
+    'axes.titlesize': 14,
+    'axes.titleweight': 'bold',
+    'axes.labelsize': 12,
+    'axes.labelweight': 'bold',
+    'axes.linewidth': 1.5,
+    'grid.color': '#999999',
+    'grid.alpha': 0.6,
+
+    # Make all text bold
+    'font.weight': 'bold',
+    'axes.labelweight': 'bold',
+    'xtick.labelsize': 11,
+    'ytick.labelsize': 11,
+    'xtick.major.width': 1.2,
+    'ytick.major.width': 1.2,
+    'xtick.color': 'black',
+    'ytick.color': 'black',
+    'legend.fontsize': 11,
+    'legend.title_fontsize': 12,
+})
